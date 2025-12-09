cfc0d5214e9747f2a59599fbcff224e955d444b3
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2025-12-09 16:36:57 -0600

    forgot to add the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Game } from './Classes/Game';
+import { SampleWorld } from './Classes/SampleWorld';
+
 const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
-const game:Game = new Game(canvas);
+const game:Game = new Game(canvas, [SampleWorld]);
 
 canvas.addEventListener("touchmove", (event) => {
   event.preventDefault();
